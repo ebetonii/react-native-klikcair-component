15ddef685e53c4b9d345f14f55cb3517d5445620
toniebe <toniebe48@gmail.com>
2022-12-03 22:58:00 +0700

    fix: dependencies and helper

--- rollup.config.js
+++ rollup.config.js
@@ -4,31 +4,39 @@ import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
 import external from "rollup-plugin-peer-deps-external";
+import dts from "rollup-plugin-dts";
 
 const packageJson = require("./package.json");
 
-export default {
-  input: "src/index.ts",
-  external: ["react", "react-native"],
-  output: [
-    {
-      file: packageJson.main,
-      format: "cjs",
-      sourcemap: true,
-      name: "react-lib",
-    },
-    {
-      file: packageJson.module,
-      format: "esm",
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    external(),
-    resolve(),
-    commonjs(),
-    typescript({ tsconfig: "./tsconfig.json" }),
-    postcss(),
-    terser(),
-  ],
-};
+export default [
+  {
+    input: "./src/index.ts",
+    external: ["react", "react-native"],
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+        name: "react-native-klikcair-component",
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      external(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
+      terser(),
+    ],
+  },
+  {
+    input: "dist/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+
+export { scale, verticalScale, moderateScale } from './helper/scaling';
+export { default as Button } from './components/Button'
+export * from './components/Button';
+
+// example export all
+// export { default as Checkbox } from './components/atoms/Checkbox';
+// export * from './components/atoms/Checkbox';
