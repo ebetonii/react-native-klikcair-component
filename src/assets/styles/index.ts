fa4af993aaece642cf3e512fd29b898fca572a42
ebetonii <ahmad.fathoni@klikcair.com>
2022-12-04 17:12:36 +0700

    fix: directory colors and change peerDepenci

--- src/assets/styles/index.ts
+++ src/assets/styles/index.ts
@@ -1,3 +1,4 @@
+
 export { default as fontSize } from './fontSize.js';
 export * from './fontSize.js';
 export * from './colors.js';
@@ -5,3 +6,51 @@ export * from './styles.js';
 export { default as font } from './font.js';
 export * from './font.js'
 export * from './size.js'
+export {
+    CNeutral,
+    CNeutral1,
+    CNeutral2,
+    CNeutral3,
+    CNeutral4,
+    CNeutral5,
+    CNeutral6,
+    CNeutral7,
+    CNeutral8,
+    CNeutral9,
+    CNeutral10,
+    CNeutral11,
+    CNeutral12,
+    CNeutral13,
+    CPrimary,
+    CPrimary1,
+    CPrimary2,
+    CPrimary3,
+    CPrimary4,
+    CSecondary,
+    CSecondary1,
+    CSecondary2,
+    CSecondary3,
+    CSecondary4,
+    CTextLight,
+    CTextLight1,
+    CTextLight2,
+    CTextLight3,
+    CTextLight4,
+    CTextLight5,
+    CTextLight6,
+    CTextLight7,
+    CTextDark,
+    CTextDark1,
+    CTextDark2,
+    CTextDark3,
+    CTextDark4,
+    CTextDark5,
+    CTextDark6,
+    CTextDark7,
+    CLink,
+    CSuccess,
+    CWarning,
+    CError,
+    backgroundColor,
+    color,
+  } from './colors.js'
