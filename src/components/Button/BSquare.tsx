823d68628374f91d025d1d2d7880a4e7146fe660
ebetonii <ahmad.fathoni@klikcair.com>
2022-12-05 07:16:19 +0700

    fix: adding Button Circle and Square

---
+++ src/components/Button/BSquare.tsx
@@ -0,0 +1,165 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import {
+  backgroundColor,
+  CNeutral5,
+  CNeutral7,
+  color,
+  CTextLight,
+  CTextLight4,
+} from "../../assets/styles/index";
+import { font } from "../../assets/styles";
+import { fontSize } from "../../assets/styles";
+import { scale } from "../../helper/scaling";
+
+export default function BSquare({
+  disable,
+  onPress,
+  icon,
+  type,
+  title,
+  children,
+  styleContainer,
+  styleText,
+  textColor,
+  buttonColor,
+}) {
+  return disable ? (
+    <View
+      style={[
+        backgroundColor.Neutral4,
+        icon && { flexDirection: "row" },
+        styles.container,
+        styles.size,
+        styleContainer,
+      ]}
+    >
+      {icon ? (
+        <Image
+          source={icon}
+          style={[styles.imageSize, { tintColor: CTextLight4 }]}
+        />
+      ) : (
+        <Text
+          style={[
+            font.Lato,
+            color.TextLight4,
+            styleText,
+            { textAlign: "center" },
+          ]}
+        >
+          {title}
+        </Text>
+      )}
+    </View>
+  ) : type === "primary" ? (
+    <TouchableOpacity
+      style={[
+        backgroundColor.Primary1,
+        styles.container,
+        icon && { flexDirection: "row" },
+        styles.size,
+        styleContainer,
+      ]}
+      onPress={onPress}
+    >
+      {icon ? (
+        <Image
+          source={icon}
+          style={[styles.imageSize, { tintColor: textColor }]}
+        />
+      ) : (
+        <Text
+          style={[
+            { color: textColor },
+            styleText,
+            font.LatoButton,
+            fontSize["button"],
+          ]}
+        >
+          {title}
+        </Text>
+      )}
+    </TouchableOpacity>
+  ) : type === "secondary" ? (
+    <TouchableOpacity
+      style={[
+        { borderWidth: 1, borderColor: buttonColor },
+        backgroundColor.Transparent,
+        styles.container,
+        icon && { flexDirection: "row" },
+        styles.size,
+        styleContainer,
+      ]}
+      onPress={onPress}
+    >
+      {icon && (
+        <Image
+          source={icon}
+          style={[styles.imageSize, { tintColor: textColor }]}
+        />
+      )}
+      <Text
+        style={[
+          { color: textColor },
+          styleText,
+          font.LatoButton,
+          fontSize["button"],
+        ]}
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  ) : type === "link" ? (
+    <TouchableOpacity
+      style={[
+        { borderWidth: 1, borderColor: buttonColor, borderStyle: "dashed" },
+        backgroundColor.Transparent,
+        styles.container,
+        icon && { flexDirection: "row" },
+        styles.size,
+        styleContainer,
+      ]}
+      onPress={onPress}
+    >
+      {icon && (
+        <Image
+          source={icon}
+          style={[styles.imageSize, { tintColor: textColor }]}
+        />
+      )}
+      <Text
+        style={[
+          { color: textColor },
+          styleText,
+          font.LatoButton,
+          fontSize["button"],
+        ]}
+      >
+        {title}
+      </Text>
+    </TouchableOpacity>
+  ) : (
+    <TouchableOpacity style={styleContainer} onPress={onPress}>
+      {children}
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // backgroundColor:'red',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  size: {
+    width: "10%",
+    minWidth: scale(40),
+    height: scale(40),
+  },
+  imageSize: {
+    width: scale(20),
+    height: scale(20),
+    resizeMode: "contain",
+  },
+});
